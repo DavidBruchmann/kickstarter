641f11278be6ddc9c99ec15551df747392dadeb0
ingmars <ingmars@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-03-05 09:26:39 +0000

    converted generated code to single quotes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kickstarter/trunk@817 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_kickstarter_sectionbase.php
+++ class.tx_kickstarter_sectionbase.php
@@ -209,8 +209,8 @@ class tx_kickstarter_sectionbase {
 	 * @return	string		The URL
 	 */
 	function linkThisCmd($uPA=array())	{
-	  $url = t3lib_div::linkThisScript($uPA);
-	  return $url;
+		$url = t3lib_div::linkThisScript($uPA);
+		return $url;
 	}
 
 	/**
@@ -229,7 +229,7 @@ class tx_kickstarter_sectionbase {
 	}
 
 	function cmdHiddenField()	{
-		return '<input type="hidden"  name="'.$this->piFieldName("cmd").'" value="'.htmlspecialchars($this->currentCMD).'">';
+		return '<input type="hidden" name="'.$this->piFieldName("cmd").'" value="'.htmlspecialchars($this->currentCMD).'">';
 	}
 
 	function preWrap($str)	{
@@ -237,6 +237,18 @@ class tx_kickstarter_sectionbase {
 		$str = '<pre>'.$str.'</pre>';
 		return $str;
 	}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ######### Functions from compilefiles #########
@@ -293,7 +305,7 @@ class tx_kickstarter_sectionbase {
 	}
 
 	function printPre($content)	{
-		echo '<pre>'.htmlspecialchars(str_replace(chr(9),"    ",$content)).'</pre>';
+		echo '<pre>'.htmlspecialchars(str_replace(chr(9),'    ',$content)).'</pre>';
 	}
 
 	function wrapBody($before,$content,$after,$indent=1)	{
@@ -302,7 +314,7 @@ class tx_kickstarter_sectionbase {
 		$parts[] = $this->indentLines(rtrim($content),$indent);
 		$parts[] = chr(10).$this->sPS($after,0);
 
-		return implode("",$parts);
+		return implode('',$parts);
 	}
 
 	function replaceMarkers($content,$markers)	{
@@ -317,12 +329,12 @@ class tx_kickstarter_sectionbase {
 	#	echo '<HR><strong>'.$name.'</strong><HR><pre>'.htmlspecialchars($content).'</pre>';
 
 		return array(
-			"name" => $name,
-			"size" => strlen($content),
-			"mtime" => time(),
-			"is_executable" => 0,
-			"content" => $content,
-			"content_md5" => md5($content)
+			'name' => $name,
+			'size' => strlen($content),
+			'mtime' => time(),
+			'is_executable' => 0,
+			'content' => $content,
+			'content_md5' => md5($content)
 		);
 	}
 
@@ -332,19 +344,19 @@ class tx_kickstarter_sectionbase {
 
 	function getSplitLabels($config,$key)	{
 		$language=array();
-		$language[]=str_replace("|","",$config[$key]);
+		$language[]=str_replace('|','',$config[$key]);
 		if (count($this->wizard->languages))	{
 			reset($this->wizard->languages);
 			while(list($lk,$lv)=each($this->wizard->languages))	{
 				if (isset($this->wizard->selectedLanguages[$lk]))	{
-					$language[]=str_replace("|","",$config[$key."_".$lk]);
-				} else $language[]="";
+					$language[]=str_replace('|','',$config[$key.'_'.$lk]);
+				} else $language[]='';
 			}
 		}
-		$out = implode("|",$language);
-		$out = str_replace(chr(10),"",$out);
-		$out = rtrim(str_replace("|",chr(10),$out));
-		$out = str_replace(chr(10),"|",$out);
+		$out = implode('|',$language);
+		$out = str_replace(chr(10),'',$out);
+		$out = rtrim(str_replace('|',chr(10),$out));
+		$out = str_replace(chr(10),'|',$out);
 		return $out;
 	}
 
@@ -363,9 +375,9 @@ class tx_kickstarter_sectionbase {
 		$lines[]='$LOCAL_LANG = Array (';
 		while(list($lK,$labels)=each($arr))	{
 			if (is_array($labels))	{
-				$lines[]='	"'.$lK.'" => Array (';
+				$lines[]="	'".$lK."' => Array (";
 				while(list($l,$v)=each($labels))	{
-					if (strcmp($v[0],""))	$lines[]='		"'.$l.'" => "'.addslashes($v[0]).'",	'.$this->WOPcomment($v[1]);
+					if (strcmp($v[0],''))	$lines[]="		'".$l."' => '".addslashes($v[0])."',	".$this->WOPcomment($v[1]);
 				}
 				$lines[]='	),';
 			}
@@ -377,12 +389,12 @@ class tx_kickstarter_sectionbase {
 
 	function writeStandardBE_xMod($extKey,$config,$pathSuffix,$cN,$k,$k_prefix)	{
 			// Make conf.php file:
-		$content = $this->sPS('
+		$content = $this->sPS("
 				// DO NOT REMOVE OR CHANGE THESE 3 LINES:
-			define("TYPO3_MOD_PATH", "ext/'.$extKey.'/'.$pathSuffix.'");
-			$BACK_PATH="../../../";
-			$MCONF["name"]="xMOD_'.$cN.'";
-		');
+			define('TYPO3_MOD_PATH', 'ext/".$extKey."/".$pathSuffix."');
+			\$BACK_PATH='../../../';
+			\$MCONF['name']='xMOD_".$cN."';
+		");
 		$content=$this->wrapBody('
 			<?php
 			',$content,'
@@ -403,31 +415,31 @@ class tx_kickstarter_sectionbase {
 		$this->addFileToFileArray($pathSuffix."clear.gif",t3lib_div::getUrl(t3lib_extMgm::extPath("kickstarter")."res/clear.gif"));
 
 			// Make module index.php file:
-		$indexContent = $this->sPS('
+		$indexContent = $this->sPS("
 				// DEFAULT initialization of a module [BEGIN]
-			unset($MCONF);
-			require ("conf.php");
-			require ($BACK_PATH."init.php");
-			require ($BACK_PATH."template.php");
-			$LANG->includeLLFile("EXT:'.$extKey.'/'.$pathSuffix.'locallang.php");
-			#include ("locallang.php");
-			require_once (PATH_t3lib."class.t3lib_scbase.php");
+			unset(\$MCONF);
+			require ('conf.php');
+			require (\$BACK_PATH.'init.php');
+			require (\$BACK_PATH.'template.php');
+			\$LANG->includeLLFile('EXT:".$extKey."/".$pathSuffix."locallang.php');
+			#include ('locallang.php');
+			require_once (PATH_t3lib.'class.t3lib_scbase.php');
 				// ....(But no access check here...)
 				// DEFAULT initialization of a module [END]
-		');
+		");
 
-		$indexContent.= $this->sPS('
-			class '.$cN.' extends t3lib_SCbase {
+		$indexContent.= $this->sPS("
+			class ".$cN." extends t3lib_SCbase {
 				/**
 				 * Adds items to the ->MOD_MENU array. Used for the function menu selector.
 				 */
 				function menuConfig()	{
-					global $LANG;
-					$this->MOD_MENU = Array (
-						"function" => Array (
-							"1" => $LANG->getLL("function1"),
-							"2" => $LANG->getLL("function2"),
-							"3" => $LANG->getLL("function3"),
+					global \$LANG;
+					\$this->MOD_MENU = Array (
+						'function' => Array (
+							'1' => \$LANG->getLL('function1'),
+							'2' => \$LANG->getLL('function2'),
+							'3' => \$LANG->getLL('function3'),
 						)
 					);
 					parent::menuConfig();
@@ -437,94 +449,94 @@ class tx_kickstarter_sectionbase {
 				 * Main function of the module. Write the content to $this->content
 				 */
 				function main()	{
-					global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
+					global \$BE_USER,\$LANG,\$BACK_PATH,\$TCA_DESCR,\$TCA,\$CLIENT,\$TYPO3_CONF_VARS;
 
 						// Draw the header.
-					$this->doc = t3lib_div::makeInstance("mediumDoc");
-					$this->doc->backPath = $BACK_PATH;
-					$this->doc->form=\'<form action="" method="POST">\';
+					\$this->doc = t3lib_div::makeInstance('mediumDoc');
+					\$this->doc->backPath = \$BACK_PATH;
+					\$this->doc->form='<form action=\"\" method=\"POST\">';
 
 						// JavaScript
-					$this->doc->JScode = \'
-						<script language="javascript" type="text/javascript">
+					\$this->doc->JScode = '
+						<script language=\"javascript\" type=\"text/javascript\">
 							script_ended = 0;
 							function jumpToUrl(URL)	{
 								document.location = URL;
 							}
 						</script>
-					\';
+					';
 
-					$this->pageinfo = t3lib_BEfunc::readPageAccess($this->id,$this->perms_clause);
-					$access = is_array($this->pageinfo) ? 1 : 0;
-					if (($this->id && $access) || ($BE_USER->user["admin"] && !$this->id))	{
-						if ($BE_USER->user["admin"] && !$this->id)	{
-							$this->pageinfo=array("title" => "[root-level]","uid"=>0,"pid"=>0);
+					\$this->pageinfo = t3lib_BEfunc::readPageAccess(\$this->id,\$this->perms_clause);
+					\$access = is_array(\$this->pageinfo) ? 1 : 0;
+					if ((\$this->id && \$access) || (\$BE_USER->user['admin'] && !\$this->id))	{
+						if (\$BE_USER->user['admin'] && !\$this->id)	{
+							\$this->pageinfo=array('title' => '[root-level]','uid'=>0,'pid'=>0);
 						}
 
-						$headerSection = $this->doc->getHeader("pages",$this->pageinfo,$this->pageinfo["_thePath"])."<br>".$LANG->sL("LLL:EXT:lang/locallang_core.php:labels.path").": ".t3lib_div::fixed_lgd_pre($this->pageinfo["_thePath"],50);
+						\$headerSection = \$this->doc->getHeader('pages',\$this->pageinfo,\$this->pageinfo['_thePath']).'<br>'.\$LANG->sL('LLL:EXT:lang/locallang_core.php:labels.path').': '.t3lib_div::fixed_lgd_pre(\$this->pageinfo['_thePath'],50);
 
-						$this->content.=$this->doc->startPage($LANG->getLL("title"));
-						$this->content.=$this->doc->header($LANG->getLL("title"));
-						$this->content.=$this->doc->spacer(5);
-						$this->content.=$this->doc->section("",$this->doc->funcMenu($headerSection,t3lib_BEfunc::getFuncMenu($this->id,"SET[function]",$this->MOD_SETTINGS["function"],$this->MOD_MENU["function"])));
-						$this->content.=$this->doc->divider(5);
+						\$this->content.=\$this->doc->startPage(\$LANG->getLL('title'));
+						\$this->content.=\$this->doc->header(\$LANG->getLL('title'));
+						\$this->content.=\$this->doc->spacer(5);
+						\$this->content.=\$this->doc->section('',\$this->doc->funcMenu(\$headerSection,t3lib_BEfunc::getFuncMenu(\$this->id,'SET[function]',\$this->MOD_SETTINGS['function'],\$this->MOD_MENU['function'])));
+						\$this->content.=\$this->doc->divider(5);
 
 
 						// Render content:
-						$this->moduleContent();
+						\$this->moduleContent();
 
 
 						// ShortCut
-						if ($BE_USER->mayMakeShortcut())	{
-							$this->content.=$this->doc->spacer(20).$this->doc->section("",$this->doc->makeShortcutIcon("id",implode(",",array_keys($this->MOD_MENU)),$this->MCONF["name"]));
+						if (\$BE_USER->mayMakeShortcut())	{
+							\$this->content.=\$this->doc->spacer(20).\$this->doc->section('',\$this->doc->makeShortcutIcon('id',implode(',',array_keys(\$this->MOD_MENU)),\$this->MCONF['name']));
 						}
 					}
-					$this->content.=$this->doc->spacer(10);
+					\$this->content.=\$this->doc->spacer(10);
 				}
 				function printContent()	{
 
-					$this->content.=$this->doc->endPage();
-					echo $this->content;
+					\$this->content.=\$this->doc->endPage();
+					echo \$this->content;
 				}
 
 				function moduleContent()	{
-					switch((string)$this->MOD_SETTINGS["function"])	{
+					switch((string)\$this->MOD_SETTINGS['function'])	{
 						case 1:
-							$content="<div align=center><strong>Hello World!</strong></div><BR>
-								The \'Kickstarter\' has made this module automatically, it contains a default framework for a backend module but apart from it does nothing useful until you open the script \'".substr(t3lib_extMgm::extPath("'.$extKey.'"),strlen(PATH_site))."'.$pathSuffix.'index.php\' and edit it!
+							\$content='<div align=center><strong>Hello World!</strong></div><BR>
+								The \"Kickstarter\" has made this module automatically, it contains a default framework for a backend module but apart from it does nothing useful until you open the script \"'.substr(t3lib_extMgm::extPath('".$extKey."'),strlen(PATH_site)).'".$pathSuffix."index.php\" and edit it!
 								<HR>
-								<BR>This is the GET/POST vars sent to the script:<BR>".
-								"GET:".t3lib_div::view_array($_GET)."<BR>".
-								"POST:".t3lib_div::view_array($_POST)."<BR>".
-								"";
-							$this->content.=$this->doc->section("Message #1:",$content,0,1);
+								<BR>This is the GET/POST vars sent to the script:<BR>'.
+								'GET:'.t3lib_div::view_array(\$_GET).'<BR>'.
+								'POST:'.t3lib_div::view_array(\$_POST).'<BR>'.
+								'';
+							\$this->content.=\$this->doc->section('Message #1:',\$content,0,1);
 						break;
 						case 2:
-							$content="<div align=center><strong>Menu item #2...</strong></div>";
-							$this->content.=$this->doc->section("Message #2:",$content,0,1);
+							\$content='<div align=center><strong>Menu item #2...</strong></div>';
+							\$this->content.=\$this->doc->section('Message #2:',\$content,0,1);
 						break;
 						case 3:
-							$content="<div align=center><strong>Menu item #3...</strong></div>";
-							$this->content.=$this->doc->section("Message #3:",$content,0,1);
+							\$content='<div align=center><strong>Menu item #3...</strong></div>';
+							\$this->content.=\$this->doc->section('Message #3:',\$content,0,1);
 						break;
 					}
 				}
 			}
-		');
+		");
 
 		$this->addFileToFileArray($pathSuffix."index.php",$this->PHPclassFile($extKey,$pathSuffix."index.php",$indexContent,$extKey.' module '.$k_prefix.$k,$cN));
 	}
 
 	function addLLfunc($extKey)	{
-		return $this->sPS('
+		return $this->sPS("
 			/**
-			 * Includes the [extDir]/locallang.php and returns the $LOCAL_LANG array found in that file.
+			 * Includes the [extDir]/locallang.php and returns the \$LOCAL_LANG array found in that file.
 			 */
 			function includeLL()	{
-				include(t3lib_extMgm::extPath("'.$extKey.'")."locallang.php");
-				return $LOCAL_LANG;
+				include(t3lib_extMgm::extPath('".$extKey."').'locallang.php');
+				return \$LOCAL_LANG;
 			}
-		');
+		");
 	}
 	function addStdLocalLangConf($ll,$k,$onlyMode=0)	{
 		$this->addLocalConf($ll,array(
@@ -620,28 +632,28 @@ class tx_kickstarter_sectionbase {
 
 		$file.="\n\n\n".$content."\n\n\n";
 
-		$file.=trim($this->sPS('
+		$file.=trim($this->sPS("
 
-			if (defined("TYPO3_MODE") && $TYPO3_CONF_VARS[TYPO3_MODE]["XCLASS"]["ext/'.$extKey.'/'.$filename.'"])	{
-				include_once($TYPO3_CONF_VARS[TYPO3_MODE]["XCLASS"]["ext/'.$extKey.'/'.$filename.'"]);
+			if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/".$extKey."/".$filename."'])	{
+				include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/".$extKey."/".$filename."']);
 			}
-			'.($SOBE_class?'
+			".($SOBE_class?"
 
 
 
 			// Make instance:
-			$SOBE = t3lib_div::makeInstance("'.$SOBE_class.'");
-			$SOBE->init();
-			'.($SOBE_extras["include"]?'
+			\$SOBE = t3lib_div::makeInstance('".$SOBE_class."');
+			\$SOBE->init();
+			".($SOBE_extras['include']?"
 			// Include files?
-			foreach($SOBE->include_once as $INC_FILE)	include_once($INC_FILE);':'').'
-			'.($SOBE_extras["firstLevel"]?'
-			$SOBE->checkExtObj();	// Checking for first level external objects':'').'
-			$SOBE->main();
-			$SOBE->printContent();
-			':'').'
+			foreach(\$SOBE->include_once as \$INC_FILE)	include_once(\$INC_FILE);":"")."
+			".($SOBE_extras['firstLevel']?"
+			\$SOBE->checkExtObj();	// Checking for first level external objects":"")."
+			\$SOBE->main();
+			\$SOBE->printContent();
+			":"")."
 			?>
-		'));
+		"));
 
 		return $file;
 	}
@@ -693,13 +705,13 @@ class tx_kickstarter_sectionbase {
 		return $EM_CONF;
 	}
 	function userField($k)	{
-	  $v = "";
-	  if($k == "name") {
-	    $v = ($GLOBALS['BE_USER']->user['realName'] != "") ? $GLOBALS['BE_USER']->user['realName'] : $this->wizard->wizArray["emconf"][1]["author"];
-	  } else if ($k == "email") {
-	    $v = ($GLOBALS['BE_USER']->user['email'] != "") ? $GLOBALS['BE_USER']->user['email'] : $this->wizard->wizArray["emconf"][1]["author_email"];
-	  }
-	  return $v;
+		$v = "";
+		if($k == "name") {
+			$v = ($this->wizard->wizArray["emconf"][1]["author"] != "") ? $this->wizard->wizArray["emconf"][1]["author"] : $GLOBALS['BE_USER']->user['realName'];
+		} else if ($k == "email") {
+			$v = ($this->wizard->wizArray["emconf"][1]["author_email"] != "") ? $this->wizard->wizArray["emconf"][1]["author_email"] : $GLOBALS['BE_USER']->user['email'];
+		}
+		return $v;
 	}
 }
 ?>
