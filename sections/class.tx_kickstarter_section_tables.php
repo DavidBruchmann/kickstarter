d42ae2118f752b4616f607d7f2f5cd43453dd2a1
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-07-24 13:29:03 +0000

    [TASK] Use mediumblob for l10n_diffsource
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kickstarter/trunk@50066 735d13b6-9817-0410-8766-e36946ffe9aa

--- sections/class.tx_kickstarter_section_tables.php
+++ sections/class.tx_kickstarter_section_tables.php
@@ -408,7 +408,7 @@ class tx_kickstarter_section_tables extends tx_kickstarter_section_fields {
 
 			$DBfields[] = 'sys_language_uid int(11) DEFAULT \'0\' NOT NULL,';
 			$DBfields[] = 'l10n_parent int(11) DEFAULT \'0\' NOT NULL,';
-			$DBfields[] = 'l10n_diffsource mediumtext,';
+			$DBfields[] = 'l10n_diffsource mediumblob,';
 
 			$columns['sys_language_uid'] = trim($this->sPS('
 				\'sys_language_uid\' => array(		'.$this->WOPcomment('WOP:'.$WOP.'[localization]').'
